9d26f9115c5d4cfff18146ae337891ce09be11e0
Pseudo <enikhil7@gmail.com>
2018-12-23 17:57:44 +0530

    Updated Gameplay Timings

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,13 +3,13 @@
     public const int POOL_SIZE = 25;
     public const int OBSTACLES_SIZE = 3;
 
-    public const float LEVEL_1_EXIT_DURATION = 5;
-    public const float LEVEL_2_EXIT_DURATION = 5;
-    public const float LEVEL_3_EXIT_DURATION = 5;
-    public const float LEVEL_4_EXIT_DURATION = 5;
-    public const float LEVEL_5_EXIT_DURATION = 5;
-    public const float LEVEL_6_EXIT_DURATION = 5;
-    public const float LEVEL_7_EXIT_DURATION = 5;
+    public const float LEVEL_1_EXIT_DURATION = 05f;
+    public const float LEVEL_2_EXIT_DURATION = 10f;
+    public const float LEVEL_3_EXIT_DURATION = 10f;
+    public const float LEVEL_4_EXIT_DURATION = 15f;
+    public const float LEVEL_5_EXIT_DURATION = 10f;
+    public const float LEVEL_6_EXIT_DURATION = 15f;
+    public const float LEVEL_7_EXIT_DURATION = 20f;
     public const float FINISH_UP_TIME = 10f;
 
     public const string BACKGROUND_TUNNEL_AUDIO = "Background";
